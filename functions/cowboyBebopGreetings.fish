e5393f0e40b15229916e68e18ca875b4e2ad40f5
gabrielfreitas-azr <gabrielfreitas.azr@gmail.com>
2022-10-12 21:48:50 -0300

    Update

---
+++ functions/cowboyBebopGreetings.fish
@@ -0,0 +1,5 @@
+function cowboyBebopGreetings
+    set file_path ~/.config/fish/functions/utils/cowboy-bebop-quotes.txt
+    set line (random 1 1 (wc -l < $file_path))
+    sed "$line q;d" $file_path
+end
